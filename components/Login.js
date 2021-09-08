951460aa21179ab8fb05a52a016a20cb22312532
SatyaSaiSrijaGottimukkala <90024089+SatyaSaiSrijaGottimukkala@users.noreply.github.com>
2021-09-08 16:29:52 +0530

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -1,15 +1,39 @@
 import React,{useState} from 'react';
+import {getEmployeeByEmail} from '../services/EmployeeData'
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
    function Login(props) {
-      const [email, setEmail] = useState('admin');
-      const [password, setPassword] = useState("admin");
+      const [email, setEmail] = useState('');
+      const [password, setPassword] = useState("");
       var handleChange = (e) =>{
          if(e.target.name === "email"){
             setEmail(e.target.value);
          }else if(e.target.name === "password"){
             setPassword(e.target.value);
          }
+        
       }
+      var func=(email,role)=>{
+       let employee=getEmployeeByEmail(email)
+       console.log(employee)
+        if (employee!==null)
+        {console.log("enter")
+            console.log("role:",role)
+            console.log("employee.role:",employee.role)
+            console.log("name:",employee.name)
+            console.log("password:",password)
+            if (role===employee.role && employee.name===password){
+                console.log("in func Admin")
+                props.history.push('/Admin');
+            }
+            else if(role===employee.role && employee.name===password){
+                console.log("in func employee")
+                props.history.push('/Normal');
+            }
+        }
+        else{
+            alert("details are wrong")
+        }}
       return (
          <div>
             <h2>Login</h2>
@@ -22,22 +46,8 @@ import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
                <Label for="examplePassword">Password</Label>
                <Input type="password" name="password" id="examplePassword"  onChange={handleChange}  value={password} placeholder="password placeholder" />
                </FormGroup>
-               <Button color="primary" onClick={()=>{
-               if(email === "admin" && password==="admin"){
-                  localStorage.setItem('user',email);
-                  props.history.push('/Admin');
-               }else{
-                  alert('Incorrect email or password. Please try again.')
-               }
-            }}>Admin</Button>
-            <Button color="primary" onClick={()=>{
-               if(email ==="employee" && password==="employee"){
-                  localStorage.setItem('user',email);
-                  props.history.push('/Normal');
-               }else{
-                  alert('Incorrect email or password. Please try again.')
-               }
-            }}>Employee</Button>
+               <Button color="primary" onClick={()=>{func(email,"admin")}}>Admin</Button>
+            <Button color="primary" onClick={()=>{func(email,"normal")}}>Employee</Button>
             </Form>
          </div>
       );
