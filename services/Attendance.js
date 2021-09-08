b739ab10f1690a61d5510196ba04b1675bf4e2d2
SatyaSaiSrijaGottimukkala <90024089+SatyaSaiSrijaGottimukkala@users.noreply.github.com>
2021-09-08 15:22:15 +0530

    Update Attendance.js

--- services/Attendance.js
+++ services/Attendance.js
@@ -1,14 +1,25 @@
 var attendance=
-[{id:111,employeeId:79323,date:"20-01-2020",inTime:"9:00",outTime:"16:00",totalHours:"7"},
-{id:112,employeeId:79324,date:"20-01-2020",inTime:"9:10",outTime:"16:30",totalHours:"7.3"},
-{id:113,employeeId:79325,date:"20-01-2020",inTime:"9:40",outTime:"17:00",totalHours:"7.15"},
-{id:114,employeeId:79326,date:"20-01-2020",inTime:"9:00",outTime:"17:00",totalHours:"8"},
-{id:115,employeeId:79327,date:"20-01-2020",inTime:"10:00",outTime:"16:00",totalHours:"6"},
-{id:116,employeeId:79323,date:"21-01-2020",inTime:"8:00",outTime:"16:00",totalHours:"8"},
-{id:117,employeeId:79324,date:"21-01-2020",inTime:"9:00",outTime:"16:30",totalHours:"7"},
-{id:118,employeeId:79325,date:"21-01-2020",inTime:"10:40",outTime:"17:00",totalHours:"6.15"},
-{id:119,employeeId:79326,date:"21-01-2020",inTime:"10:00",outTime:"17:00",totalHours:"7"},
-{id:120,employeeId:79327,date:"21-01-2020",inTime:"11:00",outTime:"18:00",totalHours:"7"}]
+[{id:111,employeeId:1,date:"20-01-2020",inTime:"9:00",outTime:"16:00",totalHours:"7"},
+{id:112,employeeId:2,date:"20-01-2020",inTime:"9:10",outTime:"16:30",totalHours:"7.3"},
+{id:113,employeeId:3,date:"20-01-2020",inTime:"9:40",outTime:"17:00",totalHours:"7.15"},
+{id:114,employeeId:4,date:"20-01-2020",inTime:"9:00",outTime:"17:00",totalHours:"8"},
+{id:115,employeeId:5,date:"20-01-2020",inTime:"10:00",outTime:"16:00",totalHours:"6"},
+{id:116,employeeId:6,date:"20-01-2020",inTime:"8:00",outTime:"16:00",totalHours:"8"},
+{id:117,employeeId:7,date:"20-01-2020",inTime:"9:00",outTime:"16:30",totalHours:"7"},
+{id:118,employeeId:8,date:"20-01-2020",inTime:"10:40",outTime:"17:00",totalHours:"6.15"},
+{id:119,employeeId:9,date:"20-01-2020",inTime:"10:00",outTime:"17:00",totalHours:"7"},
+{id:120,employeeId:10,date:"20-01-2020",inTime:"11:00",outTime:"18:00",totalHours:"7"},
+{id:121,employeeId:1,date:"21-01-2020",inTime:"9:00",outTime:"16:00",totalHours:"7"},
+{id:122,employeeId:2,date:"21-01-2020",inTime:"9:10",outTime:"16:30",totalHours:"7.3"},
+{id:123,employeeId:3,date:"21-01-2020",inTime:"9:40",outTime:"17:00",totalHours:"7.15"},
+{id:124,employeeId:4,date:"21-01-2020",inTime:"9:00",outTime:"17:00",totalHours:"8"},
+{id:125,employeeId:5,date:"21-01-2020",inTime:"10:00",outTime:"16:00",totalHours:"6"},
+{id:126,employeeId:6,date:"21-01-2020",inTime:"8:00",outTime:"16:00",totalHours:"8"},
+{id:127,employeeId:7,date:"21-01-2020",inTime:"9:00",outTime:"16:30",totalHours:"7"},
+{id:128,employeeId:8,date:"21-01-2020",inTime:"10:40",outTime:"17:00",totalHours:"6.15"},
+{id:129,employeeId:9,date:"21-01-2020",inTime:"10:00",outTime:"17:00",totalHours:"7"},
+{id:130,employeeId:10,date:"21-01-2020",inTime:"11:00",outTime:"18:00",totalHours:"7"}]
+
 
 var saveLocalStorage = () =>{
     localStorage.setItem('attendance',JSON.stringify(attendance));
